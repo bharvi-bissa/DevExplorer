b8e3188be9db190b44b9632d614f516d699399c9
bharvi-bissa <bharvi.bissa@gmail.com>
2018-08-13 23:01:41 +0530

    added edit experience and edit education functionality and components

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -66,6 +66,42 @@ export const addEducation = (eduData, history) => dispatch => {
     );
 };
 
+// delete Experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// delete Education
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // delete account and profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you Sure ? This action cannot be undone")) {
